8e5889ac6cb60d038170156b492765e772e065ca
user <user@DESKTOP-5ALULDP>
2018-10-17 12:59:49 +0200

    Utente model

--- ShopOnline/src/it/overnet/model/Utente.java
+++ ShopOnline/src/it/overnet/model/Utente.java
@@ -1,5 +1,63 @@
 package it.overnet.model;
 
 public class Utente {
-
+	private int idUtente;
+	private String nome;
+	private String cognome;
+	private String username;
+	private String password;
+	private String indirizzo;
+	
+	public Utente(int idUtente, String nome, String cognome, String username, String password, String indirizzo) {
+		super();
+		this.idUtente = idUtente;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+		this.password = password;
+		this.indirizzo = indirizzo;
+	}
+	public int getIdUtente() {
+		return idUtente;
+	}
+	public void setIdUtente(int idUtente) {
+		this.idUtente = idUtente;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	@Override
+	public String toString() {
+		return "Utente [idUtente=" + idUtente + ", nome=" + nome + ", cognome=" + cognome + ", username=" + username
+				+ ", password=" + password + ", indirizzo=" + indirizzo + "]";
+	}
+	
+	
 }
